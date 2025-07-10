2fb36b062a885df75ae1f3d64836465f885ecae6
Mnuella-leite <manuellapelizzari@gmail.com>
2025-07-10 08:44:33 -0300

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,10 @@
+print('Variaveis') 
+nome = 'Manuella'
+print('Bom dia '+nome)
+idade = 16
+print(idade)
+salario_mínimo = 1450.99
+print(salario_mínimo)
+ativo = False 
+print(ativo)
+print(type(nome))
